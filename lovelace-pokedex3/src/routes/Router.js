eaa1f4b4a42dc3201f9522e943f52ceb7be2ebfb
germanaetges <germanaetges@gmail.com>
2021-08-23 16:11:05 -0300

    fiz o router e coordinator

---
+++ lovelace-pokedex3/src/routes/Router.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PokedexPage from "../pages/PokedexPage/PokedexPage";
+import DetailsPage from "../pages/DetailsPage/DetailsPage";
+import HomePage from "../pages/HomePage/HomePage";
+
+
+const Router = () => {
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+
+        <Route exact path="/details/:name">
+          <DetailsPage />
+        </Route>
+
+        <Route exact path="/pokedex">
+          <PokedexPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
