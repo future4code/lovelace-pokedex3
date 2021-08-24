a6820459c40e502fe15e74198960de42b36a9ecb
YaraNascimento <yara.tacy.barbosa.29@gmail.com>
2021-08-24 14:50:39 -0300

    Todas as imagens e botões da home foram colocados

--- lovelace-pokedex3/src/pages/HomePage/HomePage.js
+++ lovelace-pokedex3/src/pages/HomePage/HomePage.js
@@ -1,14 +1,125 @@
 import React from 'react'
-import axios from 'axios'
 
 
 const HomePage = () => {
     return (
         <div>
-             <button>Ir para Pokedex</button>
+            <button>Ir para Pokedex</button>
             <h3>Lista de Pokémons</h3>
-         </div>
-       
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png' />
+
+            <button>Adicionar um Pokédex</button>
+            <button>Detalhes</button>
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
     )
 }
 export default HomePage
@@ -22,6 +133,5 @@ export default HomePage
 
 
 
-   
-   
-    
+
+
