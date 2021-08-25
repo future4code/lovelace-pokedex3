b7c40d2289418a1477476e6ea1141a70dad09b1b
germanaetges <germanaetges@gmail.com>
2021-08-25 15:28:20 -0300

    arrumei o router

--- lovelace-pokedex3/src/App.js
+++ lovelace-pokedex3/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react'
 import HomePage from "./pages/HomePage"
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Pokedex from './pages/Pokedex';
+import Router from "./routes/Router"
 
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-   <Switch>
-
-   <Route exact path={'/'}>
-      <HomePage />
-      </Route>
-
-    
-
-     </Switch>
-   
-
-     </BrowserRouter>
+    <Router />
   );
 }
 
